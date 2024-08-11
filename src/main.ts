0f1568271599c6b409a51628777b967ca5c495dc
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-08-11 21:04:19 +0300

    updating cors and vercel again !

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*',
+
+    methods: ['PUT', 'GET', 'PATCH', 'DELETE'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
