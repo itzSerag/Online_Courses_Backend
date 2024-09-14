a4006ddd3a3b30061fc0db6a6704bed85c1dc823
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-09-14 20:02:59 +0300

    fixing facebook/google auth

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  const whitelist = configService.get<string[]>('WHITE_LIST'); // Example: ['http://localhost:5173']
+
   // CORS
   app.enableCors({
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin || whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     allowedHeaders: '*',
     exposedHeaders: '*',
     methods: ['PUT', 'GET', 'PATCH', 'DELETE'],
