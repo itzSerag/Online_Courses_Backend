06954813942f260d072b949ce46ca359c911cea4
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-09-14 20:22:04 +0300

    adding 8080 to vercel json

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-
-  const whitelist = configService.get<string[]>('WHITE_LIST'); // Example: ['http://localhost:5173']
-
   // CORS
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || whitelist.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     allowedHeaders: '*',
     exposedHeaders: '*',
     methods: ['PUT', 'GET', 'PATCH', 'DELETE'],
