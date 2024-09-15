32d90e88437d240cfa7abd719b65cb1fcc8ddb9f
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-09-15 15:53:25 +0300

    making it 127.0.0.1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:8080' });
+  app.enableCors({
+    origin: 'http://127.0.0.1:8080',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
