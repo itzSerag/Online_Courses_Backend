39f22318be037f956f8fed5c951a11ab23bcf4b2
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-09-15 15:56:24 +0300

    origin * now

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:8080',
+    origin: '*',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
